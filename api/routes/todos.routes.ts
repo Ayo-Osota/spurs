25f08af4bc80fc8795a5a8376b24c315c97fa2e9
Ayo-Osota <osotaayomikun@gmail.com>
2024-12-16 09:54:45 +0100

    `Removed unused import and function, refactored code for todos controller and routes`

--- api/routes/todos.routes.ts
+++ api/routes/todos.routes.ts
@@ -1,9 +1,7 @@
-import express, { Request, Response } from "express";
-import { checkID, createTodo, deleteTodo, editTodo, getAllTodos, getTodo } from '../controllers/todos.controller';
+import express from "express";
+import { createTodo, deleteTodo, editTodo, getAllTodos, getTodo } from '../controllers/todos.controller';
 
 const todosRouter = express.Router();
-
-todosRouter.param('id', checkID)
 
 todosRouter.route('/').get(getAllTodos).post(createTodo)
 todosRouter.route('/:id').get(getTodo).patch(editTodo).delete(deleteTodo)
