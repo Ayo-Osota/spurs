fc689367e338aab500ad8fa72e9caf4c8663e862
Ayo Osota <osotaayomikun@gmail.com>
2025-01-08 07:00:45 +0100

    refactor: comment out database connection middleware in app.ts

--- api/app.ts
+++ api/app.ts
@@ -8,18 +8,18 @@ const app = express()
 // Middleware
 app.use(express.json())
 
-app.use('/api', async (req, res, next) => {
-    try {
-        await connectDb()
-        next()
-    } catch (error) {
-        console.error('Database connection error:', error)
-        res.status(500).json({
-            status: 'fail',
-            message: 'Database connection failed',
-        })
-    }
-})
+// app.use('/api', async (req, res, next) => {
+//     try {
+//         await connectDb()
+//         next()
+//     } catch (error) {
+//         console.error('Database connection error:', error)
+//         res.status(500).json({
+//             status: 'fail',
+//             message: 'Database connection failed',
+//         })
+//     }
+// })
 
 // app.use((req, res, next) => {
 //     next()
